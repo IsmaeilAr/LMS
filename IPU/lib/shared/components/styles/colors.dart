9b32c78be0b6ed5359b1054e7dc5e80a8c841a2b
IsmaeilAr <101134539+IsmaeilAr@users.noreply.github.com>
2022-03-08 01:43:34 +0200

    Add files via upload

--- IPU/lib/shared/components/styles/colors.dart
+++ IPU/lib/shared/components/styles/colors.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
 
+const Color navyBlue = Color.fromRGBO(0x03, 0x04, 0x1D, 1.0);
+const Color infoNavyBlue = Color.fromRGBO(0x8B, 0x97, 0xFC, 1.0);
+const Color lightBlue = Color.fromRGBO(0x63, 0x60, 0xFF, 1.0);
+const Color infoLightBlue = Color.fromRGBO(0xFD, 0x97, 0x97, 1.0);
+const Color blue = Color.fromRGBO(0x3E, 0x3B, 0xCE, 1.0);
+const Color infoBlue = Color.fromRGBO(0x6A, 0xD7, 0xC3, 1.0);
+const Color orange = Color.fromRGBO(0xF7, 0x82, 0x22, 1.0);
+const Color infoOrange = Color.fromRGBO(0xF7, 0x82, 0x22, 0.5);
+const Color pink = Color.fromRGBO(0xAF, 0xAF, 0xAF, 1.0);
+const Color infoPink = Color.fromRGBO(0xAF, 0xAF, 0xAF, 0.5);
+const Color black = Color.fromRGBO(0x00, 0x00, 0x00, 1.0);
+const Color white = Color.fromRGBO(0xFF, 0xFF, 0xFF, 1.0);
+const Color grey = Color.fromRGBO(0xF3, 0xF4, 0xF6, 1.0);
+// const Color grey = Colors.grey;
