a47836b2b564363ab65accb855c8a0e4e4e3d649
IsmaeilAr <101134539+IsmaeilAr@users.noreply.github.com>
2022-03-08 01:40:21 +0200

    Add files via upload

--- IPU/lib/main.dart
+++ IPU/lib/main.dart
@@ -1,1 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:ipu/layout/intro.dart';
 
+void main() {
+
+  runApp( const MyApp());
+}
+
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      title: 'IPU',
+      theme: ThemeData(
+        fontFamily: 'Urbanest'
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const Intro(),
+    );
+  }
+}
